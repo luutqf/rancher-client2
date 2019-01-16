627bb2d6166e88818482c177de183c81ac0e4148
luutqf <zai_wu_chun_qiu@163.com>
2019-01-16 09:24:33 +0800

    create

---
+++ src/main/java/cn/luutqf/rancher/client/RancherClient2Application.java
@@ -0,0 +1,14 @@
+package cn.luutqf.rancher.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RancherClient2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RancherClient2Application.class, args);
+    }
+
+}
+
---
+++ src/main/java/cn/luutqf/rancher/client/service/package-info.java
@@ -0,0 +1,1 @@
+package cn.luutqf.rancher.client.service;
